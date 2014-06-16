628d5897729ae887f9d72096034197de00a5c81b
krasnobaev <alekseykrasnobaev@gmail.com>
2014-06-17 03:07:06 +0400

    initial implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() - encapsulates access to cached and raw matrix
+## cacheSolve()      - solve matrix with aware of cache
 
-## Write a short comment describing this function
+## processed and raw matrix encapsulation
+## return - list of set/get functions to access matrix
+##   rawm - raw matrix
+makeCacheMatrix <- function(rawm = matrix()) {
+    procm <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+    ## raw matrix encapsulation
+    set <- function(a) {
+        rawm  <<- a
+        procm <<- NULL
+    }
+    get <- function() rawm
+    
+    ## solved matrix encapsulation
+    setProcessed <- function(b) procm <<- b
+    getProcessed <- function()  procm
 
+    list(set = set, get = get,
+         setProcessed = setProcessed,
+         getProcessed = getProcessed)
 }
 
-
-## Write a short comment describing this function
-
+## solve matrix with aware of cache and write time matrix
+## been processed
+## return - matrix that is the inverse of x
+##      x - list returned by makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    t <- proc.time()
+
+    ## check whether matrix is processed
+    m <- x$getProcessed()
+    if(!is.null(m)) {
+        message("getting cached data")
+        message("Process Time: ", proc.time() - t)
+        return(m)
+    }
+
+    ## cache miss
+    m <- solve(x$get(), ...)
+    x$setProcessed(m)
+    message("Process Time: ", proc.time() - t)
+
+    m
 }
